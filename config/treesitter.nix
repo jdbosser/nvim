74946665c5d02bb8466ab6004bd450419898b6b7
Daniel Bossér <daniel.bosser@liu.se>
2024-07-12 16:04:39 +0200

    improve treesitter

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -5,6 +5,34 @@
 		
 		enable = true; 
 		nixvimInjections = true;
+		
+		# From the nixvim documentation
+		# https://nix-community.github.io/nixvim/plugins/treesitter/settings/index.html
+
+		settings = {
+		  auto_install = false;
+		  ensure_installed = [
+			"c" "lua" "vim" "vimdoc" "markdown" "markdown_inline" "python" "rust"
+		  ];
+		  highlight = {
+		    enable = true;
+		  };
+		  incremental_selection = {
+		    enable = true;
+		    keymaps = {
+		      init_selection = "gnn";
+		      node_decremental = "grm";
+		      node_incremental = "grn";
+		      scope_incremental = "grc";
+		    };
+		  };
+		  indent = {
+		    enable = true;
+		  };
+		  parser_install_dir = "$HOME/.config/nvim/treesitter";
+		  sync_install = false;
+		};
+
 	};
 
 }
